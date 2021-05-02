5d85142582098903ef53c354b95cbe5dec3f35b3
Ross <conover.ross@gmail.com>
2021-05-02 11:29:25 -0500

    setup the React App

---
+++ gbc-workshop-app/src/App.js
@@ -0,0 +1,14 @@
+import './CSS/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Oh, mostly dang</p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
